7d010f25de6eb237432aeec60797fad61b327a2e
Luna <contact@pupyrinth.dev>
2025-04-01 03:42:22 +0200

    Seperate screenshot

--- Scripts/screenshot-swappy.sh
+++ Scripts/screenshot-swappy.sh
@@ -1,8 +1,10 @@
 screenshotName="$(date +%Y%m%d_%H%m%s)"
 screenshotDir="$HOME/myHome/Pictures/Screenshots"
-  
+
 hyprctl keyword animation "fadeOut, 0, 0, default"
 
-grimblast --freeze copysave area "${screenshotDir}/$screenshotName.png"
+grimblast --freeze save area "${screenshotDir}/$screenshotName.png" 
+
+swappy -f "${screenshotDir}/$screenshotName.png" -o "${screenshotDir}/$screenshotName-Swappy.png"
 
 hyprctl keyword animation "fadeOut, 1, 4, default"
